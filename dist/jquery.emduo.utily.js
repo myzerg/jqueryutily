d4af7b73b5e73548b2d6b9be26e3fee7c7abd793
myzerg <myzerg@126.com>
2017-04-20 14:04:03 +0800

    fix a ...

---
+++ dist/jquery.emduo.utily.js
@@ -0,0 +1,15 @@
+/*!
+ * Copyright 2017-2017 emduo, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
+
+if (typeof jQuery === 'undefined') {
+  throw new Error('emduo jqury utily lib requires jQuery')
+}
+
+"use strict";
+$.isPhone = function(s) 
+{
+	var tel_preg = /^(13[0-9]|14[0-9]|15[0-9]|17[0-9]|18[0-9])\d{8}$/i;
+	return tel_preg.test(s);
+}
